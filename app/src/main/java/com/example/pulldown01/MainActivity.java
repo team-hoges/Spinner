165d1c679ffe9c53410d5ab7166d281978cdafae
ShokiOkauchi <s15513@salesio-sp.ac.jp>
2019-10-18 10:58:03 +0900

    プルダウン作成完了

---
+++ app/src/main/java/com/example/pulldown01/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pulldown01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner=findViewById(R.id.plain_spinnar);
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.plain_array,R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
